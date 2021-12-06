80567dfc79175ad7a94532074cdd21abd8e6d27a
mudit <muditshukla010@gmail.com>
2021-12-06 22:25:54 +0530

    completed linear search

--- src/searching/LinearSearch.java
+++ src/searching/LinearSearch.java
@@ -8,4 +8,18 @@
 package searching;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 60, 50, 30, 100};
+        int targetValue = 1000;
+        boolean flag = false;
+        for(int i = 0; i< array.length; i++){
+            if(array[i] == targetValue){
+                flag = true;
+                System.out.println("Element found at " + (i+1) + " position");
+                break;
+            }
+        }
+        if(!flag)
+            System.out.println("Element not found");
+    }
 }
