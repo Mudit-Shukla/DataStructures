0e6717961eb8f0df13bda09848d2913850f71d4b
mudit <muditshukla010@gmail.com>
2021-12-06 22:18:10 +0530

    added file for linear search

---
+++ src/searching/LinearSearch.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mudit Shukla
+ *   Date: 06-12-2021
+ *   Time: 22:17
+ *   File: LinearSearch.java
+ */
+
+package searching;
+
+public class LinearSearch {
+}
